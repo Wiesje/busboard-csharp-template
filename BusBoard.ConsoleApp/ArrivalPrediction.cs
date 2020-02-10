eef5ad65dd2da04e99770261fc28325980d70787
wiesje van den Heerik <wiesje.heerik@softwire.com>
2020-02-10 16:09:55 +0000

    Part 1

---
+++ BusBoard.ConsoleApp/ArrivalPrediction.cs
@@ -0,0 +1,9 @@
+﻿namespace BusBoard
+{
+    public class ArrivalPrediction
+    {
+        public string LineName { get; set; }
+        public string DestinationName { get; set; }
+        public int TimeToStation { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace BusBoard
 {
@@ -6,7 +9,29 @@ namespace BusBoard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var stopId = PromptForStopId();
+
+            var predictions = new TflApi().GetArrivalPredictions(stopId);
+            var predictionsToDisplay = predictions.OrderBy(p => p.TimeToStation).Take(5);
+            DisplayPredictions(predictionsToDisplay);
+
+            Console.ReadLine();
+        }
+
+        private static string PromptForStopId()
+        {
+            Console.Write("Enter your stop ID: ");
+            return Console.ReadLine(); // Example: "490008660N"
+        }
+
+        private static void DisplayPredictions(IEnumerable<ArrivalPrediction> predictionsToDisplay)
+        {
+            foreach (var prediction in predictionsToDisplay)
+            {
+                Console.WriteLine($"{prediction.TimeToStation/60} minutes: {prediction.LineName} to {prediction.DestinationName}");
+            }
         }
     }
 }
